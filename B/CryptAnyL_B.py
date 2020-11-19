8c6fbbe6206ab76056071d993b93977130865679
Konstantin <const.perfect@gmail.com>
2020-11-19 13:42:14 +0300

    Initial commit

---
+++ B/CryptAnyL_B.py
@@ -0,0 +1,225 @@
+# CryptAnyL project from nebuhtit
+#
+from CryptAnyL_modules import *
+who = 'BB'
+print('-h = list of commands; список команд')
+
+def uslovia(inputt):
+    # Comands for input
+    global pasw, pubkeyFromFile
+    what_to_do = ''
+    if str(inputt) == '-h' or str(inputt) == '-help':
+        print("""
+-q = quit of program; выход из програмы
+-h or -help = list of commands; список команд
+-mypublic = your public key; Ваши публичные ключи
+-newkeys = create new privat and public keys encrypted by password; Создайте новые приватные и публичные ключи зашифрованные паролем
+While chat:
+    [enter] = Pass this part; Пропустить это действие
+        """)
+        what_to_do = 'continue'
+
+    if str(inputt) == '-newkeys':
+        aaaaa =True
+        while aaaaa == True:
+            pasw = input('Create new password:')
+            if str(pasw) == '-h' or str(pasw) == '-newkeys' or str(pasw) == '-q' or str(pasw) == '-mypublic':
+                uslovia(str(pasw))
+                continue
+            else:
+                createNewkeys('BB', pasw)
+                pubkeyFromFile = b64in(dec_F_import(pasw, 'publicresBB.txt'))
+                print('created')
+                what_to_do = 'continue'
+                # return what_to_do
+                # continue
+                break
+    if str(inputt) == '-q':
+        quit()
+
+    if str(inputt) == '-mypublic':
+        q = True
+
+        while q == True:
+            try:
+                pasw = input('Enter password:')
+                pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres' + who + '.txt'))
+                # print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+                q0 = False
+                what_to_do = 'break'
+                print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+                break
+                # return pasw, what_to_do
+            except FileNotFoundError as e:
+                print(extract_tb(exc_info()[2])[0][1], e)
+                pasw = input('Keys are not exist. Create new password:')
+                createNewkeys(who, pasw)
+                pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres' + who + '.txt'))
+                q0 = False
+                what_to_do = 'break'
+                break
+                # return pasw, what_to_do
+            except Exception as e:
+                print(extract_tb(exc_info()[2])[0][1], e)
+                print('password is not correct or file with mistake, try again.')
+                what_to_do = 'continue'
+                continue
+    return what_to_do
+        # continue
+q = True
+#First check for existing privat key in file _
+try:
+    open('personalresBB.txt','r')
+except FileNotFoundError as e:
+        #pasw, what_to_do = ifif('-newkeys', who)
+        uslovia('-newkeys')
+        q = False
+        # print(extract_tb(exc_info()[2])[0][1], e)
+        # pubkey, privkey = newKeys()
+        # pasw = input('Create new password:')
+        # enc_F_save(pasw, privkey, 'personalresBB.txt')
+        # enc_F_save(pasw, pubkey, 'publicresBB.txt')
+        # pubkeyFromFile = b64in(dec_F_import(pasw, 'publicresBB.txt'))
+        # #print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+        # q = False
+except Exception as e:
+    print(extract_tb(exc_info()[2])[0][1], e)
+q0 = True
+while q0 == True:
+    try:
+        pasw = input('Enter password:')
+        usl = uslovia(pasw)
+        if usl == 'continue':
+            continue
+        elif usl == 'break':
+            break
+        # if str(pasw) == '-newkeys':
+        #     pasw = input('Create new password:')
+        #     createNewkeys('BB', pasw)
+        #     pubkeyFromFile = b64in(dec_F_import(pasw, 'publicresBB.txt'))
+        #     continue
+        # if str(pasw) == '-q':
+        #     quit()
+        # if str(pasw) == '-mypublic':
+        #     q = True
+        #     while q == True:
+        #         try:
+        #             pasw = input('Enter password:')
+        #             pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres' + who + '.txt'))
+        #             #print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+        #             q0 = False
+        #             what_to_do = 'continue'
+        #             break
+        #             # return pasw, what_to_do
+        #         except FileNotFoundError as e:
+        #             print(extract_tb(exc_info()[2])[0][1], e)
+        #             pasw = input('Keys are not exist. Create new password:')
+        #             createNewkeys(who, pasw)
+        #             pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres' + who + '.txt'))
+        #             q0 = False
+        #             what_to_do = 'continue'
+        #             break
+        #             # return pasw, what_to_do
+        #         except Exception as e:
+        #             print(extract_tb(exc_info()[2])[0][1], e)
+        #             print('password is not correct or file with mistake, try again.')
+        #             continue
+
+        privatkey = dec_F_import(pasw, 'personalresBB.txt')
+        q0 = False
+        break
+    except NameError as e:
+        print(extract_tb(exc_info()[2])[0][1], e)
+        pasw = input('Enter password:')
+        continue
+    except Exception as e:
+        print(extract_tb(exc_info()[2])[0][1], e)
+        print('password is not correct or file with mistake, try again.')
+        continue
+# while q == True:
+#     try:
+#         if q0 == False:
+#             break
+#         else:
+#             pasw = input('Enter password:')
+#         if str(pasw) == '-newkeys':
+#             createNewkeys('BB')
+#         pubkeyFromFile = b64in(dec_F_import(pasw, 'publicresBB.txt'))
+#         q = False
+#         break
+#     except Exception as e:
+#         print(extract_tb(exc_info()[2])[0][1], e)
+#         print(
+#             'password is not correct or file with mistake, try again.\n-newkeys recreate your keys but you loos your priveus messages')  # Дописать возможность создания новых публичных ключей со стиранием старых.
+#         continue
+pubkeyFromFile = b64in(dec_F_import(pasw, 'publicresBB.txt'))
+q2 = True
+
+print('Your public key:\n', pubkeyFromFile, '\nsent it to your friend')
+
+while q2 == True:
+    try:
+        INP_Fkeys = dec_F_import(pasw, 'friendsresBB.txt')
+        q2 = False
+    except Exception as e:
+        print(extract_tb(exc_info()[2])[0][1], e)
+
+        INP_Fkeys = re.sub(r'\s', '', input("Past here friend's public key\n:"))
+
+
+        usl = uslovia(INP_Fkeys)
+        if usl == 'continue':
+            continue
+        elif usl == 'break':
+            break
+
+    enc_F_save(pasw, INP_Fkeys, 'friendsresBB.txt')
+    privatkey = dec_F_import(pasw, 'personalresBB.txt')
+    break
+
+
+
+
+
+q3 = True
+while q3 == True:
+    try:
+        INP_sent = input("Write here your message:")
+
+        usl = uslovia(INP_sent)
+        if usl == 'continue':
+            enc_F_save(pasw, INP_Fkeys, 'friendsresBB.txt')
+            privatkey = dec_F_import(pasw, 'personalresBB.txt')
+            print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+            continue
+        elif usl == 'break':
+            continue
+        if str(INP_sent) == '':
+            pass
+        else:
+            INP_Fkeys = b64out(INP_Fkeys)
+            #print("INP_Fkeys:", INP_Fkeys)
+            myEncryptM = encry(INP_sent, INP_Fkeys)
+            INP_Fkeys = b64in(INP_Fkeys)
+
+            print('Sent this text to your friend:',myEncryptM)
+
+        #INP_Fmessage = input('past here friends encrypted message\n:')
+        INP_Fmessage = input('Past here friends encrypted message\n:')
+        if INP_Fmessage == '':
+            continue
+        usl = uslovia(INP_Fmessage)
+        if usl == 'continue':
+            enc_F_save(pasw, INP_Fkeys, 'friendsresBB.txt')
+            privatkey = dec_F_import(pasw, 'personalresBB.txt')
+            print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+            continue
+        elif usl == 'break':
+            continue
+        else:
+            INP_Fmessage = re.sub(r'\s', '', INP_Fmessage)
+            F_encrypted_m = decry(INP_Fmessage, privatkey)
+            print(F_encrypted_m)
+    except Exception as e:
+        print(extract_tb(exc_info()[2])[0][1],e)
+        continue
---
+++ CryptAnyL_modules.py
@@ -0,0 +1,210 @@
+#AnyLetter  18112020_
+import rsa
+import base64
+import re
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from sys import exc_info
+from traceback import extract_tb
+
+def newKeys():
+    # Creates new keys
+    (pubkey, privkey) = rsa.newkeys(512)
+    pubkey = re.sub(r'[^\,\d]', '', str(pubkey))
+    #pubkey = [int(item) for item in pubkey]
+    privkey = re.sub(r'[^\,\d]', '', str(privkey))
+    #privkey = [int(item) for item in privkey]
+    return pubkey, privkey
+#pubkey, privkey = newKeys()
+#print(pubkey,privkey)
+
+def b64in(textin):
+    # func convert by base64 (to str)
+    t = str(textin).encode('utf8')
+    ress = base64.b64encode(t)
+    res = ress.decode('utf8')
+    return res
+def b64out(textin2):
+    t2 = str(textin2).encode('utf8')
+    ress2 = base64.b64decode(t2)
+    res2 = ress2.decode('utf8')
+    return res2
+# proverka64 = b64in('123rfs4')
+# print(proverka64)
+# proverka64out = b64out(proverka64)
+# print(proverka64out)
+
+def pub():
+        return pubkey
+
+# def priv():
+#     return privkey
+
+def encry(m, pub):
+    pub = [int(item) for item in pub.split(',')]
+    #print(pub)
+    pub = rsa.PublicKey(pub[0],pub[1])
+    #print(pub)
+    # function encrypt message by public key
+    # For working needs utf
+    m_= m.encode('utf8')
+    # General crypt part
+    crypto_m = rsa.encrypt(m_, pub)
+    # print(crypto_m) #how it looks in original
+    # For str we need to convert it from bytes
+    aa = base64.b64encode(crypto_m)
+    # Return decode utf8. You can turn on or turn off this part.
+    a = aa.decode('utf8')
+    return a
+#p = encry('Првыапыв52346654№;%:;:?%?:%,', pubkey)
+#print(p)
+
+def decry(em, priv):
+    priv = [int(item) for item in priv.split(',')]
+    priv = rsa.PrivateKey(priv[0],priv[1],priv[2],priv[3],priv[4])
+    # func decrypt message by privat key
+    em_ = em.encode('utf8')
+    b = base64.b64decode(em_)
+    decrypto_em = rsa.decrypt(b, priv)
+    # print(decrypto_em)
+    d_m = decrypto_em.decode('utf8')
+    return d_m
+#res = decry(p, privkey)
+#print(res)
+# print(pub())
+
+
+def encrypting(i_password, text):
+    # Encrypting str by password
+    text = text.encode('utf8')
+    iu_password = i_password.encode('utf-8')
+    password = iu_password
+    salt = b'\xcfTQN\xd3\xb1\x1c\x96\x9eg\xe4\x82\xd2\xa3>!'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    # print("key: ", key)
+    f = Fernet(key)
+    # print("f: ", f)
+    # text = input('Please, write text: ').encode('utf-8')
+    token = f.encrypt(text)
+    # print("token: ", token)
+    t_k = base64.b64encode(token)
+    t_k1 = t_k.decode('utf-8')
+    t_k2 = str(t_k1)
+    # print("text_k2: ", t_k2)
+    return salt, t_k2
+
+# salt, t_k2 = encrypting('123','hello')
+
+def decrypting(i_password, enc_text):
+    # Decripting previos encrypted text by the same password
+    iu_password = i_password.encode('utf-8')
+    password = iu_password
+    salt = b'\xcfTQN\xd3\xb1\x1c\x96\x9eg\xe4\x82\xd2\xa3>!'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    o_enc_t = enc_text
+    encod_enc_t = o_enc_t.encode('utf-8')
+    b_encod_enc_t = base64.b64decode(encod_enc_t)
+    # print("b_encod_enc_t: ", b_encod_enc_t)
+
+    decryptedtext = f.decrypt(b_encod_enc_t)
+    # print("decryptedtext: ", decryptedtext)
+    decryptedtext = decryptedtext.decode('utf8')
+    return decryptedtext
+# decryptedtext = decrypting('123',t_k2)
+# print(decryptedtext)
+
+def enc_F_save(password, stri, name):
+    # Encrypted by password str saves in file.txt
+    password = str(password)
+    stri = str(stri)
+    salt, encryptedtext = encrypting(password, stri)
+    f = open(str(name), 'w')
+    f.write(str(encryptedtext))
+#enc_F_save('123', privkey)
+
+def dec_F_import(password, file):
+    # Load data from file and decrypts it by password
+    password = str(password)
+    ff = open(file, 'r')
+    f = ff.read()
+    decryptedtext = decrypting(password, f)
+    return decryptedtext
+# de = dec_F_import('123', 'file.txt')
+# print(de)
+
+def createNewkeys(NIK, pasw):
+    # Func create new public and privat keys and rewrites files with this keys
+    NIK =str(NIK)
+    global pubkey, privkey, pubkeyFromFile
+    pubkey, privkey = newKeys()
+    #pasw = input('Create new password:')
+    enc_F_save(pasw, privkey, str('personalres'+NIK+'.txt'))
+    enc_F_save(pasw, pubkey, str('publicres'+NIK+'.txt'))
+    pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres'+NIK+'.txt'))
+    #print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+
+def ifif(inp, wh):
+    # DOESN'T WORK
+    # Comands for input
+    wh = str(wh)
+    global q, q0, pasw, pasw_
+    if str(inp) == '-q':
+        quit()
+    if str(inp) == '-mypublic':
+        q = True
+        while q ==True:
+            try:
+                pasw = input('Enter password:')
+                pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres'+wh+'.txt'))
+                print('Your public key:', pubkeyFromFile, '\nsent it to your friend')
+                q0 = False
+                what_to_do = 'continue'
+                #return pasw, what_to_do
+            except FileNotFoundError as e:
+                print(extract_tb(exc_info()[2])[0][1], e)
+                pasw = input('Keys are not exist. Create new password:')
+                createNewkeys(wh, pasw)
+                pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres'+wh+'.txt'))
+                q0 = False
+                what_to_do = 'continue'
+                #return pasw, what_to_do
+            except Exception as e:
+                print(extract_tb(exc_info()[2])[0][1], e)
+                print('password is not correct or file with mistake, try again.')
+                continue
+
+
+            return what_to_do
+    if str(inp) == '-newkeys':
+        pasw = input('Create new password:')
+        createNewkeys(wh, pasw)
+        pubkeyFromFile = b64in(dec_F_import(pasw, 'publicres'+str(wh)+'.txt'))
+        pasw_ = pasw
+        q = False
+        q0 = False
+        what_to_do = 'break'
+        return pasw, what_to_do
+
+
+#ifif('-newkeys','AA')
+
+
+
